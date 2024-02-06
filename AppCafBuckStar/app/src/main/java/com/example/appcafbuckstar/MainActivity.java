36f73c72301b180b09952e696f4b3d85a26e7f5e
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2024-02-06 18:00:05 -0500

    tp1 started

---
+++ AppCafBuckStar/app/src/main/java/com/example/appcafbuckstar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.appcafbuckstar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//BUILD SUPPRIME FOLDER POUR LA REMISE
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- true_project_2/app/src/main/java/com/example/application_annexe2/MainActivity.java
+++ true_project_2/app/src/main/java/com/example/application_annexe2/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
 import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +19,11 @@ public class MainActivity extends AppCompatActivity {
     //1. initialiser variables globalement
 
     Ecouteur ec;
-    Button boutonValider;
     EditText champNomCompte;
     TextView champSolde;
     EditText receveur;
+
+    Spinner spin;
 
     EditText sommeTransfert;
     Button boutonEnvoyer;
@@ -26,14 +31,17 @@ public class MainActivity extends AppCompatActivity {
     int solde;
 
     Vector <String> v;
+    DecimalFormat df;
+
+    String nomCompte;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        boutonValider = findViewById(R.id.valider);
-        champNomCompte = findViewById(R.id.typeCompte);
+        spin = findViewById(R.id.spinnerNomCompte);
         champSolde = findViewById(R.id.solde);
         receveur = findViewById(R.id.receveur);
         sommeTransfert = findViewById(R.id.transfertSomme);
@@ -47,31 +55,30 @@ public class MainActivity extends AppCompatActivity {
         //1.2 créer ec (classe en dernier)!
         ec = new Ecouteur();
         //2. inscription de la source a l'ecouter
-        boutonValider.setOnClickListener(ec);
         boutonEnvoyer.setOnClickListener(ec);
+
+        df = new DecimalFormat("0.00$");
+
+        //configurer le spinner (dropdownlist)
+        //creer array adapter & linker avec spinner
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                v);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(ec);
+
 
 
 
     }
     //3. definir la classe ecouteur (classe interne ici)
-    private class Ecouteur implements View.OnClickListener {
+    private class Ecouteur implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
         @Override
         public void onClick(View source) { //button here will be passed as parameter as object
-            if(source==boutonValider){
+//                String nom = champNomCompte.getText().toString();
+//                nom = nom.toUpperCase();
 
-                String nom = champNomCompte.getText().toString();
-                nom = nom.toUpperCase();
-
-
-                if(v.contains(nom)){
-                    champSolde.setText(Integer.toString(solde));
-                }
-                else {
-                    champSolde.setText("pas le bon compte");
-                }
-
-            }
             if(source==boutonEnvoyer){
                 String goodreceveur = receveur.getText().toString().trim();
                 if(goodreceveur.length()!=0){ //si pas vide
@@ -83,8 +90,21 @@ public class MainActivity extends AppCompatActivity {
                     receveur.setText("Indiquer un destinataire");
                 }
             }
+        }
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            //position: 0 , 1, 2 lequel qu'on a select
+            TextView textView = (TextView) view;
+                    //pcw item des spinner c des textview
+            nomCompte = textView.getText().toString();
+            //ou: nomCompte = v.get(position);
 
 
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
 
         }
     }
